1ce16d51f3b21fcc350c1cc48f18fb835c849fdd
koike.shinji <koike.shinji@nifty.co.jp>
2017-08-18 17:44:17 +0900

    clean code

--- examples/main.tf
+++ examples/main.tf
@@ -1,14 +1,10 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {}
-
-provider "ncl" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
-}
+provider "ncl" {}
 
 resource "ncl_instance" "sample" {
   image_id = "68"
+  key_name = "OjtKoike"
+  instance_type = "mini"
+  avail_zone = "west-12"
+  accounting_type = "2"
+  instance_id = "OjtTerra"
 }
-
--- ncl/resource_instance.go
+++ ncl/resource_instance.go
@@ -1,7 +1,10 @@
 package ncl
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/higebu/go-niftycloud/compute"
 )
 
 func resourceInstance() *schema.Resource {
@@ -16,93 +19,98 @@ func resourceInstance() *schema.Resource {
 				Required: true,
 				//ForceNew: true,
 			},
-			//	"key_name": &schema.Schema{
-			//		Type:     schema.TypeString,
-			//		Required: true,
-			//	},
-			//	"instance_type": &schema.Schema{
-			//		Type:     schema.TypeString,
-			//		Required: true,
-			//	},
-			//	"avail_zone": &schema.Schema{
-			//		Type:     schema.TypeString,
-			//		Required: true,
-			//	},
-			//			"security_groups": &schema.Schema{
-			//				Type: schema.TypeList,
-			//				//Required: true,
-			//				Optional: true,
-			//				//MiniItms: 1,
-			//				Elem: &schema.Resource{
-			//					Schema: map[string]*schema.Schema{
-			//						"id": &schema.Schema{
-			//							Type:     schema.TypeString,
-			//							Required: true,
-			//						},
-			//						"name": &schema.Schema{
-			//							Type:     schema.TypeString,
-			//							Optional: true,
-			//						},
-			//					},
-			//				},
-			//			},
-			//			"user_data": &schema.Schema{
-			//				Type:     schema.TypeString,
-			//				Optional: true,
-			//			},
-			//			"disable_api_termination": &schema.Schema{
-			//				Type:     schema.TypeBool,
-			//				Optional: true,
-			//				Default:  false,
-			//			},
-			//			"accounting_type": &schema.Schema{
-			//				Type:     schema.TypeString,
-			//				Required: true,
-			//			},
-			//			"instance_id": &schema.Schema{
-			//				Type:     schema.TypeString,
-			//				Required: false,
-			//			},
-			//			"admin": &schema.Schema{
-			//				Type:     schema.TypeString,
-			//				Optional: true,
-			//			},
-			//			"password": &schema.Schema{
-			//				Type:     schema.TypeString,
-			//				Optional: true,
-			//			},
-			//			"ip_type": &schema.Schema{
-			//				Type:     schema.TypeString,
-			//				Optional: true,
-			//			},
-			//			"public_ip": &schema.Schema{
-			//				Type:     schema.TypeString,
-			//				Optional: true,
-			//			},
-			//			"agreement": &schema.Schema{
-			//				Type:     schema.TypeString,
-			//				Optional: true,
-			//			},
+			"key_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"instance_type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"avail_zone": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"security_groups": &schema.Schema{
+				Type: schema.TypeList,
+				//Required: true,
+				Optional: true,
+				//MiniItms: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"name": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"user_data": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"disable_api_termination": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"accounting_type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"instance_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				//ValidateFunc: func(data interface{}, name string) {
+				//	// TODO: validate
+				//	fmt.Println("validation")
+				//},
+			},
+			"admin": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"password": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"ip_type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"public_ip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"agreement": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 		},
 	}
 }
 
 func resourceInstanceCreate(d *schema.ResourceData, meta interface{}) error {
-	//nclClient := meta.(*NclClient)
+	nclClient := meta.(*NclClient)
 
-	//opts := compute.RunInstancesOptions{
-	//	ImageId:      d.Get("image_id").(string),
-	//	KeyName:      d.Get("key_name").(string),
-	//	InstanceType: d.Get("instance_type").(string),
-	//	//SecurityGroups: []compute.SecurityGroup{{Name: "examplegroup"}},
-	//	AvailZone:      d.Get("avail_zone").(string),
-	//	AccountingType: d.Get("accounting_type").(string),
-	//}
-	//_, err := nclClient.RunInstances(&opts)
+	opts := compute.RunInstancesOptions{
+		ImageId:      d.Get("image_id").(string),
+		KeyName:      d.Get("key_name").(string),
+		InstanceType: d.Get("instance_type").(string),
+		//SecurityGroups: []compute.SecurityGroup{{Name: "OjtKoike"}},
+		AvailZone:      d.Get("avail_zone").(string),
+		AccountingType: d.Get("accounting_type").(string),
+		InstanceId:     d.Get("instance_id").(string),
+	}
+	_, err := nclClient.RunInstances(&opts)
 
-	//if err != nil {
-	//	return fmt.Errorf("Error completing tasks: %#v", err)
-	//}
+	if err != nil {
+		return fmt.Errorf("Error completing tasks: %#v", err)
+	}
 
 	//	d.SetId(d.Get("external_ip").(string) + ":" + portString + " > " + d.Get("internal_ip").(string) + ":" + translatedPortString)
 	return nil
