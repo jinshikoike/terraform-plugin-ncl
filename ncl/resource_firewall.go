ad6f78dd0000cce245b30a542608eb601d930a4c
koike.shinji <koike.shinji@nifty.co.jp>
2017-09-22 17:31:39 +0900

    create firewall resource

--- ncl/resource_firewall.go
+++ ncl/resource_firewall.go
@@ -14,7 +14,7 @@ func resourceFireWall() *schema.Resource {
 		Update: resourceFireWallUpdate,
 		Delete: resourceFireWallDelete,
 		Schema: map[string]*schema.Schema{
-			"group_name": &schema.Schema{
+			"name": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
@@ -31,17 +31,16 @@ func resourceFireWall() *schema.Resource {
 func resourceFireWallCreate(d *schema.ResourceData, meta interface{}) error {
 	nclClient := meta.(*NclClient)
 
-  opts := compute.ImportKeyOpts{
-    KeyName: d.Get("name").(string),
-    PublicKeyMaterial: d.Get("public_key_material").(string),
+  opts := compute.SecurityGroup{
+    Name: d.Get("name").(string),
     Description: d.Get("description").(string),
   }
 
-	resp, err := nclClient.ImportKeyPair(&opts)
+	resp, err := nclClient.CreateSecurityGroup(opts)
 	if err != nil {
 		return fmt.Errorf("Error completing tasks: %#v", err)
 	}
-	d.SetId(resp.KeyName)
+	d.SetId(resp.Name)
 	return nil
 }
 
@@ -51,20 +50,26 @@ func resourceFireWallDelete(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	keyName := d.Id()
-	if keyName == "" {
+	groupName := d.Id()
+	if groupName == "" {
 		return nil
 	}
 
-  _, deleteErr := nclClient.DeleteKeyPair(keyName)
+  // TODO: Before delete security groups, you must remove applied server from securityg group apply
+
+  opts := compute.SecurityGroup{
+    Name: groupName,
+  }
+  _, deleteErr := nclClient.DeleteSecurityGroup(opts)
 	if deleteErr != nil {
-		return fmt.Errorf("Error Delete Key Pair error: %s", deleteErr)
+		return fmt.Errorf("Error Delete Security Group error: %s", deleteErr)
 	}
+
 	return resourceFireWallRead(d, meta)
 }
 
 func resourceFireWallUpdate(d *schema.ResourceData, meta interface{}) error {
-  //nclClient := meta.(*NclClient)
+  // nclClient := meta.(*NclClient)
 	keyName := d.Id()
 	if keyName == "" {
 		return nil
@@ -76,30 +81,30 @@ func resourceFireWallUpdate(d *schema.ResourceData, meta interface{}) error {
 func resourceFireWallRead(d *schema.ResourceData, meta interface{}) error {
 	nclClient := meta.(*NclClient)
 
-	keyName := d.Id()
-	if keyName == "" {
-		keyName = d.Get("name").(string)
+	groupName := d.Id()
+	if groupName == "" {
+		groupName = d.Get("name").(string)
 	}
 
-  resp, err := nclClient.KeyPairs([]string{keyName}, nil)
-  if err != nil {
-    fmt.Println("KeyPairs request failed")
+  group := compute.SecurityGroup{
+    Name: groupName,
   }
 
-  var keyPairObj *compute.KeyPair
-  for _, val := range resp.Keys {
-    if val.Name == keyName {
-        keyPairObj = &val
-        break
-    }
+  resp, err := nclClient.SecurityGroups([]compute.SecurityGroup{group}, nil)
+	if err != nil {
+    return fmt.Errorf("Error SecurityGroups request : %#v", err)
   }
 
-  if keyPairObj == nil {
+  if len(resp.Groups) <= 0 {
     d.SetId("")
     return nil
   }
 
-	d.Set("name", keyPairObj.Name)
+  //fmt.Println(resp.Groups[0].IPPerms)
+	d.Set("name", resp.Groups[0].Name)
+  d.Set("description", resp.Groups[0].Description)
+
+  d.SetId(resp.Groups[0].Name)
 
 	return nil
 }
